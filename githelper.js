39ff9c7e092b552ffbf97052e7643fc469de10f1
okuoku <mjt@cltn.org>
2018-09-10 03:16:36 +0900

    check

---
+++ githelper.js
@@ -0,0 +1,29 @@
+function getmainhistory(commit, count){
+    var cur = commit;
+    var ret = [];
+
+    return new Promise(done => {
+        function next(){
+            if(count == 0){
+                done(ret);
+            }else{
+                cur.getParents().then(arr => {
+                    if(arr && arr[0]){
+                        ret.push(arr);
+                        count--;
+                        cur = arr[0];
+                        next();
+                    }else{
+                        done(ret);
+                    }
+                });
+            }
+        };
+
+        next();
+    });
+};
+
+module.exports = {
+    getmainhistory:getmainhistory
+};
